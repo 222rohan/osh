865073a9a27727009cd783892e2f24234809a408
222rohan <rohanshenoy405@gmail.com>
2024-11-10 20:47:30 +0530

    r added documentation and basic shell functionalities till single prog exec

---
+++ include/shell.h
@@ -0,0 +1,117 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
+// shell constants
+#define MAX_BUFFER_LEN 1024        // Maximum length of a command
+#define MAX_ARG_COUNT 64        // Maximum number of arguments per command
+#define INTERACTIVE 1           // Interactive mode
+#define RUN_SUCCESS 1           // Command run successfully
+#define RUN_FAILURE -1              // General failure
+#define RUN_CMD_NOTFOUND -2             // Command not found
+
+extern int is_interactive;                      // Flag to indicate whether the shell is in interactive mode
+extern int last_exit_status;                      // Exit status of the last executed command/binary
+extern char *SH_PATH;                          // Path to the shell executable
+extern char *builtins[];                        // Built-in commands
+extern int (*builtins_fn[])(char **);              // Built-in command functions
+
+//formatting
+#define RESET "\033[0m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN "\033[36m"
+#define ORANGE "\033[38;5;208m"
+
+// MAIN SHELL FUNCTIONS
+/*
+run_shell_loop
+    Main interactive shell loop,
+    which displays the shell prompt and reads input from the user.
+    then runs the command.
+*/
+void run_shell_loop(void);                          // Main interactive shell loop
+
+/*
+prompt
+    Display the shell prompt (e.g., osh>)
+    todo: add the current working directory to the prompt
+*/
+void prompt(void);                      // Display the shell prompt (e.g., osh>
+
+// THE SHELL UTILS
+
+/*
+read_line
+    Read a line of input from the user.
+    The line is stored in the buffer.
+    Returns 0 if the user enters an empty line.(ie. whitespace)
+    else returns 1.
+*/
+int read_line(char *);               
+
+/*
+run_line
+    Run a command.
+    The command is passed as a string.
+    The command is parsed and executed.
+    returns the exit status of the command,
+    or -1 if the command is not valid.
+*/           
+int run_line(char *);  
+
+/*
+parse_input
+    Parse the input line.
+    tokenizes the input line into arguments.
+    returns the number of arguments.
+    TODO: checks for redirection, piping, and multiple commands.
+*/                        
+int parse_line(char *, char **);        
+
+/*
+get_builtin
+    Check if the command is a built-in command.
+    returns the index of the built-in command.
+    returns -1 if the command is not a built-in command.
+*/
+int get_builtin(char *);    
+
+/*
+get_shell_dir
+    helper function,
+    returns the directory path of the shell executable.
+*/
+char *get_shell_dir(void);
+
+/*
+exec_fn_builtin
+    Execute a built-in command,
+    which is a function pointer,,
+    with index bi_index.
+    The command is passed as a string.
+    returns the exit status of the command.
+*/
+int exec_fn_builtin(int, char **); 
+
+/*
+exec_cmd_ext
+    Execute an external command.
+    The command is passed as a string.
+    if absolute path is not provided,
+     the command is searched in the PATH.
+    else the absolute path is executed.
+    returns the exit status of the command.
+*/
+int exec_cmd_ext(char **);
+
+#endif // SHELL_H
---
+++ shell/shell.c
@@ -0,0 +1,170 @@
+//main driver for the shell
+
+//contains all definitions for the shell
+#include "../include/shell.h"
+
+//shell constants
+int is_interactive = INTERACTIVE;
+int last_exit_status = 0;
+char *SH_PATH;
+
+//built-in commands
+char *builtins[] = {"cd", "exit"};
+// int (*builtins_fn[])(char **) = {&bi_cd, &bi_exit};
+
+//function definitions
+
+/* checks if arguments are passed to the shell
+    if arguments are passed, the shell is not in interactive mode
+    and the arguments are executed as commands
+    if no arguments are passed, the shell is in interactive mode
+    and user enters commands through stdin
+*/
+int main(int argc, char **argv) {
+
+    SH_PATH = get_shell_dir();
+
+    if(argc > 1) {
+        is_interactive = !INTERACTIVE;
+        //do this later
+    }
+
+    run_shell_loop();
+    return 0;
+}
+
+//main shell loop
+void run_shell_loop(void) {
+    char line[MAX_BUFFER_LEN] = {0};
+    int run_status = 0;
+
+    while (1) {
+        prompt();
+        if(read_line(line)) {   
+            run_status = run_line(line);
+            if(run_status == RUN_CMD_NOTFOUND) {
+                printf("%sosh: %scommand not found%s\n", RED, YELLOW, RESET);
+            }
+        }
+        memset(line, 0, MAX_BUFFER_LEN);
+    }
+}
+
+void prompt(void) {
+    if (is_interactive) {
+        char current_dir[MAX_BUFFER_LEN];
+        getcwd(current_dir, sizeof(current_dir));
+        printf("%s(%s):%sosh> %s", ORANGE, current_dir, CYAN ,RESET);
+    }
+}
+
+int read_line(char *line) {
+    if (fgets(line, MAX_BUFFER_LEN, stdin) == NULL) {
+        printf("\n");
+        return 0;
+    }
+    int len = strlen(line);
+    if (line[len - 1] == '\n') {
+        line[len - 1] = '\0';
+    }
+    return 1;
+}
+
+int run_line(char *line) {
+    char *args[MAX_ARG_COUNT];
+    int arg_count = parse_line(line, args);
+    if (arg_count == 0) {
+        return RUN_FAILURE;
+    }
+    int bi_index = get_builtin(args[0]);
+    if (bi_index != -1) {
+        return exec_fn_builtin(bi_index, args);
+    }
+
+    return exec_cmd_ext(args);
+}
+
+int parse_line(char *line, char **args) {
+    int i = 0;
+    args[i] = strtok(line, " ");
+    while (args[i] != NULL) {
+        i++;
+        args[i] = strtok(NULL, " ");
+    }
+    return i;
+}
+
+int get_builtin(char *cmd) {
+    for (int i = 0; i < 0; i++) {
+        if (strcmp(cmd, builtins[i]) == 0) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int exec_fn_builtin(int bi_index, char **args) {
+    //return builtins_fn[bi_index](args);
+    return 0;
+}
+
+char *get_shell_dir() {
+    static char shell_dir[MAX_BUFFER_LEN];  // Store the shell's directory path
+    ssize_t len = readlink("/proc/self/exe", shell_dir, sizeof(shell_dir) - 1);
+    if (len == -1) {
+        perror("readlink");
+        exit(EXIT_FAILURE);
+    }
+    shell_dir[len] = '\0'; 
+
+    //remove executable name from the path
+    char *last_slash = strrchr(shell_dir, '/');
+    if (last_slash != NULL) {
+        *last_slash = '\0';  
+    }
+    return shell_dir;
+}
+
+int exec_cmd_ext(char **args) {
+    char cmd_path[1024];
+
+//case 1: absolute path is provided
+    if (args[0][0] == '/') {
+
+        pid_t pid = fork();
+        if (pid == 0) {
+            if (execvp(args[0], args) == -1) {
+                perror("execvp");
+                exit(RUN_CMD_NOTFOUND);
+            }
+        } else if (pid < 0) {
+            perror("fork");
+            return RUN_FAILURE;
+        } else {
+            int status;
+            waitpid(pid, &status, 0);
+            last_exit_status = WEXITSTATUS(status);
+        }
+
+        return RUN_SUCCESS;
+    }
+
+//case 2: search in PATH
+    snprintf(cmd_path, sizeof(cmd_path), "%s/%s", SH_PATH, args[0]);
+
+    pid_t pid = fork();
+    if (pid == 0) {
+        if (execvp(cmd_path, args) == -1) {
+            return RUN_CMD_NOTFOUND;
+        }
+    } else if (pid < 0) {
+        perror("fork");
+        return RUN_FAILURE;
+    } else {
+        int status;
+        waitpid(pid, &status, 0);
+        last_exit_status = WEXITSTATUS(status);
+    }
+
+    return RUN_SUCCESS;
+}
